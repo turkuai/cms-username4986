843fde0b19500b64107a774f7208eba761023a67
Silja Koulu <silja.koulu@kukkula.local>
2025-05-20 12:02:36 +0300

    yeah idk

--- admin/db.php
+++ admin/db.php
@@ -1,14 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nettisivu";
+foreach (parse_ini_file('.env') as $key => $value) {
+    $_ENV[$key] = $value;
+  }
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+$conn = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 ?>
